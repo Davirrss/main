74bf0771857d96bec46b6bcbe80717676af50ec9
davi rodrigues <davihx09@gmail.com>
2023-05-09 17:27:44 -0300

    update

--- Exercicios/exercicios_html_js/main.js
+++ Exercicios/exercicios_html_js/main.js
@@ -1,12 +1,11 @@
-const form = document.getElementById('form');
-const num1 = document.getElementById('num1');
-const num2 = document.getElementById('num2');
+const form = document.getElementById('num1');
 
-form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    if (parseFloat(num2.value) > parseFloat(num1.value)) {
-        alert("Sucesso!");
-    } else {
-        alert("Falhou!");
+const form = document.getElementById('num2');
+
+form.addEventListener('submit', function(e){
+    if (num2 > num1){
+        alert('Sucesso!')
+    }else {
+        alert('Falhou!')
     }
-});
+})
