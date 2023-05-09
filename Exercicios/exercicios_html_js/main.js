3800ef0b01bab0a0b36361c25938b817a4a62ce1
Good Miimo <davihx09@gmail.com>
2023-05-09 19:47:50 -0300

    update

--- Exercicios/exercicios_html_js/main.js
+++ Exercicios/exercicios_html_js/main.js
@@ -1,11 +1,16 @@
-const form = document.getElementById('num1');
-
-const form = document.getElementById('num2');
-
-form.addEventListener('submit', function(e){
-    if (num2 > num1){
-        alert('Sucesso!')
-    }else {
-        alert('Falhou!')
+function compararNumeros() {
+    const valorA = parseFloat(document.getElementById("num1").value);
+    const valorB = parseFloat(document.getElementById("num2").value);
+    
+    if (valorB > valorA) {
+        alert("Valor B é maior que Valor A.");
+    } else {
+        alert("Valor A é maior ou igual a Valor B.");
     }
-})
+}
+
+    const form = document.querySelector('form');
+    form.addEventListener('submit', function(e) {
+    e.preventDefault(); // evita que o formulário seja enviado
+    compararNumeros();
+    });
