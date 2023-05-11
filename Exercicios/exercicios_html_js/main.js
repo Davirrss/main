7ec52f580d9bf7a4aeeac4354e0a949ac28e1164
davi rodrigues <davihx09@gmail.com>
2023-05-11 17:06:47 -0300

    update

--- Exercicios/exercicios_html_js/main.js
+++ Exercicios/exercicios_html_js/main.js
@@ -13,20 +13,33 @@ function compararNumeros() {
     return [valorA, valorB, resultadoDiv];
 }
 
+
 const form = document.querySelector('form');
+
 const attempts = document.getElementById("tentativas");
+attempts.style.display = "none";
+
 const resultadoDiv = document.getElementById("resultado");
 resultadoDiv.style.display = "none";
-attempts.style.display = "none";
+
+const botaoApagar = document.getElementById("apagar");
+botaoApagar.style.display = "none"
 
 let tenta = 0;
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
     tenta += 1;
+    botaoApagar.style.display = "block"
     attempts.style.display = "block";
-    attempts.textContent = `Quantidade de vezes que você fez os testes nesses turnos: ${tenta}`;
+    attempts.textContent = `Quantidade de vezes que você fez os testes nesse turno: ${tenta}`;
     const [valorA, valorB, resultadoDiv] = compararNumeros();
     resultadoDiv.style.display = "block";
     form.reset();
+});
+
+botaoApagar.addEventListener("click", function() {
+    resultadoDiv.style.display = "none";
+    attempts.style.display = "none"
+    botaoApagar.style.display = "none"
 });
