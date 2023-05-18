17d3ea18aab1da50a463e44179e143304762e354
davi rodrigues <davihx09@gmail.com>
2023-05-18 17:07:31 -0300

    update

--- Trabalhos/projeto_principal_jshtmlcss/main.js
+++ Trabalhos/projeto_principal_jshtmlcss/main.js
@@ -10,36 +10,49 @@ bentrar.addEventListener('click', function() {
         notiuser.textContent = "Você ainda não informou o nome e senha"
         document.body.appendChild(notiuser);
         resetedosvalores();
+        msgtemp(notiuser, 3000);
     } else if (nameuser == '') {
         const notiuser = document.createElement("div");
         notiuser.classList = ("errodiv");
         notiuser.textContent = "Nome de usuário não informado";
         document.body.appendChild(notiuser);
         resetedosvalores();
+        msgtemp(notiuser, 3000);
     }else if (passworduser == '') {
         const notiuser = document.createElement("div");
         notiuser.classList = ("errodiv");
         notiuser.textContent = "Senha não informada";
         document.body.appendChild(notiuser);
         resetedosvalores();
+        msgtemp(notiuser, 3000);
+    }else if(verificacaoespaco(nameuser)){
+        const notiuser = document.createElement("div");
+        notiuser.classList = ("errodiv");
+        notiuser.textContent = "O nome de usuário não pode conter espaços";
+        document.body.appendChild(notiuser);
+        resetedosvalores();
+        msgtemp(notiuser, 3000);
     }else if (nameuser.length <= 4) {
         const notiuser = document.createElement("div");
         notiuser.classList = ("errodiv");
         notiuser.textContent = "Seu nome de usuário não pode conter menos de 4 digitos.";
         document.body.appendChild(notiuser);
         resetedosvalores();
+        msgtemp(notiuser, 3000);
     }else if (caraespecial(nameuser)) {
         const notiuser = document.createElement("div");
         notiuser.classList = ("errodiv");
         notiuser.textContent = "O nome de usuário não pode conter números ou caracteres especiais";
         document.body.appendChild(notiuser);
         resetedosvalores();
+        msgtemp(notiuser, 3000);
     }else {
         const notiuser = document.createElement("div");
         notiuser.classList = ("certodiv");
         notiuser.textContent = "sucesso";
         document.body.appendChild(notiuser);
         resetedosvalores();
+        msgtemp(notiuser, 4000);
         const blink = document.createElement("button");
         blink.textContent = "botão";
         blink.id = "botaoredi";
@@ -57,4 +70,20 @@ function resetedosvalores() {
 
 function caraespecial(usuario) {
     return /[^\w\s]/.test(usuario);
+}
+
+function msgtemp(conteudo, tempo) {
+    setTimeout(() => {
+        conteudo.remove();
+    }, tempo);
+}
+
+function verificacaoespaco(stringnome) {
+    const nomefuncao = stringnome.split('');
+    for (let i = 0; i < nomefuncao.length; i++) {
+        if (nomefuncao[i] === ' ') {
+            return true;
+        }
+    }
+    return false;
 }
