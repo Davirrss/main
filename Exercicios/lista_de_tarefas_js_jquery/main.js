6fee9dc4c64004bcc8ff485aaf222fe103ff926a
davi rodrigues <davihx09@gmail.com>
2023-05-19 16:57:17 -0300

    update

---
+++ Exercicios/lista_de_tarefas_js_jquery/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    const badicionar = $("#adicionar");
+    badicionar.on('click', function(e) {
+        e.preventDefault();
+        listatarefa();
+    });
+
+    const inputarefa = $('#inputarefa');
+    inputarefa.on('keypress', function(event) {
+    if (event.which === 13) {
+        event.preventDefault();
+        listatarefa();
+    }
+
+    });
+    function listatarefa() {
+        const nometarefa = $('#inputarefa').val().trim();
+        if (nometarefa === '') {
+            const errodiv = $('<div></div>').attr('id', 'erroaviso').text('Campo vazio');
+            $('#erroaviso').remove(); 
+            $('#inputarefa').after(errodiv);
+        } else {
+            $('#erroaviso').remove();
+            const tarefalista = $('<li></li>').text(nometarefa);
+            $('#tarefalist').append(tarefalista);
+            $('#inputarefa').val('');
+            const bselecionado = $("<button></button>").attr('class', 'botaoselecionar').text('X');
+            tarefalista.append(bselecionado);
+            bselecionado.on('click', function() {
+                const comparacao = $(this).parent();
+                if (comparacao.hasClass('concluido')) {
+                comparacao.removeClass('concluido');
+                } else {
+                comparacao.addClass('concluido');
+                }
+            });
+        }
+    }
+});
+
+// $(this).parent().toggleClass('linhaconcluida');
