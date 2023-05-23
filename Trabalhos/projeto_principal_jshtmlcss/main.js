380d6fdee5401bbb9e67cfd84106105bcaed2c34
davi rodrigues <davihx09@gmail.com>
2023-05-23 17:21:20 -0300

    update

--- Trabalhos/projeto_principal_jshtmlcss/main.js
+++ Trabalhos/projeto_principal_jshtmlcss/main.js
@@ -1,89 +1,99 @@
-let nameuser = '', passworduser = '';
-const bentrar = document.getElementById("botaoentrar");
+$(document).ready(function () {
+    const bvisivel = $("#visivel");
 
-bentrar.addEventListener('click', function() {
-    nameuser = document.getElementById("nome").value;
-    passworduser = document.getElementById("senha").value;
-    if (nameuser == '' && passworduser == '') {
-        const notiuser = document.createElement("div");
-        notiuser.classList = ("errodiv");
-        notiuser.textContent = "Você ainda não informou o nome e senha"
-        document.body.appendChild(notiuser);
-        resetedosvalores();
-        msgtemp(notiuser, 3000);
-    } else if (nameuser == '') {
-        const notiuser = document.createElement("div");
-        notiuser.classList = ("errodiv");
-        notiuser.textContent = "Nome de usuário não informado";
-        document.body.appendChild(notiuser);
-        resetedosvalores();
-        msgtemp(notiuser, 3000);
-    }else if (passworduser == '') {
-        const notiuser = document.createElement("div");
-        notiuser.classList = ("errodiv");
-        notiuser.textContent = "Senha não informada";
-        document.body.appendChild(notiuser);
-        resetedosvalores();
-        msgtemp(notiuser, 3000);
-    }else if(verificacaoespaco(nameuser)){
-        const notiuser = document.createElement("div");
-        notiuser.classList = ("errodiv");
-        notiuser.textContent = "O nome de usuário não pode conter espaços";
-        document.body.appendChild(notiuser);
-        resetedosvalores();
-        msgtemp(notiuser, 3000);
-    }else if (nameuser.length <= 4) {
-        const notiuser = document.createElement("div");
-        notiuser.classList = ("errodiv");
-        notiuser.textContent = "Seu nome de usuário não pode conter menos de 4 digitos.";
-        document.body.appendChild(notiuser);
-        resetedosvalores();
-        msgtemp(notiuser, 3000);
-    }else if (caraespecial(nameuser)) {
-        const notiuser = document.createElement("div");
-        notiuser.classList = ("errodiv");
-        notiuser.textContent = "O nome de usuário não pode conter números ou caracteres especiais";
-        document.body.appendChild(notiuser);
-        resetedosvalores();
-        msgtemp(notiuser, 3000);
-    }else {
-        const notiuser = document.createElement("div");
-        notiuser.classList = ("certodiv");
-        notiuser.textContent = "sucesso";
-        document.body.appendChild(notiuser);
-        resetedosvalores();
-        msgtemp(notiuser, 4000);
-        const blink = document.createElement("button");
-        blink.textContent = "botão";
-        blink.id = "botaoredi";
-        document.body.appendChild(blink);
-        blink.addEventListener('click', function() {
-            window.location.href = "www.google.com";
+    bvisivel.on('click', function () {
+        const inputsenha = $("#senha");
+        inputsenha.toggleClass('ativado');
+    })
+
+    let nameuser = '', passworduser = '';
+    const bentrar = $("#botaoentrar");
+
+    bentrar.on('click', function () {
+        nameuser = $("#nome").val();
+        passworduser = $("#senha").val();
+
+        if (nameuser === '' && passworduser === '') {
+            const notiuser = $("<div></div>").addClass("errodiv").text("Você ainda não informou o nome e senha");
+            $(".logindireita").append(notiuser);
+            resetedosvalores();
+            botaofechar(notiuser);
+            msgtemp(notiuser, 10000);
+        } else if (nameuser === '') {
+            const notiuser = $("<div></div>").addClass("errodiv").text("Nome de usuário não informado");
+            $(".logindireita").append(notiuser);
+            resetedosvalores();
+            botaofechar(notiuser);
+            msgtemp(notiuser, 10000);
+        } else if (passworduser === '') {
+            const notiuser = $("<div></div>").addClass("errodiv").text("Senha não informada");
+            $(".logindireita").append(notiuser);
+            resetedosvalores();
+            botaofechar(notiuser);
+            msgtemp(notiuser, 10000);
+        } else if (verificacaoespaco(nameuser)) {
+            const notiuser = $("<div></div>").addClass("errodiv").text("O nome de usuário não pode conter espaços");
+            $(".logindireita").append(notiuser);
+            resetedosvalores();
+            botaofechar(notiuser);
+            msgtemp(notiuser, 10000);
+        } else if (passworduser.length <= 4) {
+            const notiuser = $("<div></div>").addClass("errodiv").text("Sua senha não pode conter menos de 4 digitos.");
+            $(".logindireita").append(notiuser);
+            resetedosvalores();
+            botaofechar(notiuser);
+            msgtemp(notiuser, 10000);
+        } else if (nameuser.length <= 4) {
+            const notiuser = $("<div></div>").addClass("errodiv").text("Seu nome de usuário não pode conter menos de 4 digitos.");
+            $(".logindireita").append(notiuser);
+            resetedosvalores();
+            botaofechar(notiuser);
+            msgtemp(notiuser, 10000);
+        } else if (caraespecial(nameuser)) {
+            const notiuser = $("<div></div>").addClass("errodiv").text("Seu nome de usuário não pode conter números ou caracteres especiais");
+            $(".logindireita").append(notiuser);
+            resetedosvalores();
+            botaofechar(notiuser);
+            msgtemp(notiuser, 10000);
+        } else {
+            resetedosvalores();
+            window.location.href = "/Trabalhos/projeto_principal_jshtmlcss/interacao.html"
+        }
+    });
+
+    function resetedosvalores() {
+        $("#nome").val("");
+        $("#senha").val("");
+    }
+
+    function caraespecial(usuario) {
+        return /[^\w\s]/.test(usuario);
+    }
+
+    function msgtemp(conteudo, tempo) {
+        setTimeout(() => {
+            conteudo.remove();
+        }, tempo);
+    }
+
+    function verificacaoespaco(stringnome) {
+        const nomefuncao = stringnome.split('');
+        for (let i = 0; i < nomefuncao.length; i++) {
+            if (nomefuncao[i] === ' ') {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    function botaofechar(notificacao) {
+        const bnotiuser = $("<button></button>").addClass("bfecharnoti").text("X");
+        notificacao.append(bnotiuser);
+        bnotiuser.on('click', function () {
+            notificacao.addClass('temporemocao')
+            notificacao.on('transitionend', function() {
+                notificacao.remove();
+            })
         })
     }
 })
-
-function resetedosvalores() {
-    document.getElementById("nome").value = "";
-    document.getElementById("senha").value = "";
-}
-
-function caraespecial(usuario) {
-    return /[^\w\s]/.test(usuario);
-}
-
-function msgtemp(conteudo, tempo) {
-    setTimeout(() => {
-        conteudo.remove();
-    }, tempo);
-}
-
-function verificacaoespaco(stringnome) {
-    const nomefuncao = stringnome.split('');
-    for (let i = 0; i < nomefuncao.length; i++) {
-        if (nomefuncao[i] === ' ') {
-            return true;
-        }
-    }
-    return false;
-}
