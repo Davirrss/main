343f5486eaef5be067b9977c1f30a099d121ae82
davi rodrigues <davihx09@gmail.com>
2023-05-25 17:23:32 -0300

    udapte

--- Trabalhos/projeto_principal_jshtmlcss/interacao.js
+++ Trabalhos/projeto_principal_jshtmlcss/interacao.js
@@ -3,21 +3,17 @@ $(document).ready(function () {
     var username = params.get('usuario');
 
     const nomeperfil = $("<div></div>").addClass("nomediv").text(username);
-    $(".topo").append(nomeperfil);
+    $(".userposi").append(nomeperfil);
 
-    const badicionar = $("#badicionar");
-    badicionar.on('click', function () {
-        const caixa = $("<div></div>").addClass("projetouser");;
-        const inputnome = $("<input>").addClass("inputnome");
-        $(caixa).append(inputnome);
-        $(".container").append(caixa);
-        const inputimagem = $('<input type="file"> ')
-        const imagem = $('<img src="choose.png">')
-        $(caixa).append(imagem, inputimagem)
-        input.onchange = (e) => {
-            if (input.files[0])
-                img.src = URL.createObjectURL(input.files[0]);
-            
-        };
+    $('#bmostrar').click(function(){
+        $('.userconfig').slideToggle();
+        var iconeMostrar = $('#iconeMostrar');
+        if (iconeMostrar.attr('src') === '/Trabalhos/projeto_principal_jshtmlcss/imagens/app-1646211_1280.png') {
+            iconeMostrar.attr('src', '/Trabalhos/projeto_principal_jshtmlcss/imagens/app-1646212_1280.png');
+        } else {
+            iconeMostrar.attr('src', '/Trabalhos/projeto_principal_jshtmlcss/imagens/app-1646211_1280.png');
+        }
     });
+
+
 });
