975921fc2c7a0554e11ed5d30f86f63b1d2095ea
davi rodrigues <davihx09@gmail.com>
2023-05-30 17:30:36 -0300

    update

--- Trabalhos/projeto_principal_jshtmlcss/interacao.js
+++ Trabalhos/projeto_principal_jshtmlcss/interacao.js
@@ -5,15 +5,25 @@ $(document).ready(function () {
     const nomeperfil = $("<div></div>").addClass("nomediv").text(username);
     $(".userposi").append(nomeperfil);
 
-    $('#bmostrar').click(function(){
+    $('#bmostrar').click(function () {
         $('.userconfig').slideToggle();
-        var iconeMostrar = $('#iconeMostrar');
-        if (iconeMostrar.attr('src') === '/Trabalhos/projeto_principal_jshtmlcss/imagens/app-1646211_1280.png') {
-            iconeMostrar.attr('src', '/Trabalhos/projeto_principal_jshtmlcss/imagens/app-1646212_1280.png');
-        } else {
-            iconeMostrar.attr('src', '/Trabalhos/projeto_principal_jshtmlcss/imagens/app-1646211_1280.png');
-        }
+        $(this).toggleClass('ativadomostrar');
     });
 
+    $('.ophistoriabotao button').click(function () {
+        var btarget = $(this).data('target');
+        var reptarget = $('.' + btarget);
+
+        if (reptarget.is(':visible')) {
+            reptarget.addClass('text-blur-out');
+            setTimeout(function(){
+                reptarget.hide();
+            }, 1500)
+        } else {
+            $('.opativado > div').hide();
+            reptarget.removeClass('text-blur-out');
+            reptarget.show().addClass('text-focus-in');
+        }
+    });
 
 });
