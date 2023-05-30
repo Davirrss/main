f8a61a0c4c3c3ea0d441ad28882568e37cc60f09
Good Miimo <davihx09@gmail.com>
2023-05-30 06:44:44 -0300

    update

--- Exercicios/calculadora/main.js
+++ Exercicios/calculadora/main.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     let cravar = 0
     let numerostringantes = ''
     let numerostringdepois = ''
-
+    botaofuncao();
 
     $('#resultado').click(function () {
         for (let i = 0; i < valor.length; i++) {
@@ -54,91 +54,91 @@ $(document).ready(function () {
             }
         }
     });
-
-    $('#clear').click(function () {
-        $('.ixibicao').remove();
-        $('.resultado2').remove();
-        const novoresultado = $('<div class="resultado2"></div>');
-        const novaixibicao = $('<div class="ixibicao"></div>');
-        $('.vaga').append(novaixibicao, novoresultado);
-        valor = [];
-        numerostringantes = '';
-        numerostringdepois = '';
-    })
-
-    $('#divisao').click(function () {
-        valor.push("divisao");
-        $('.ixibicao').append("/");
-    })
-
-    $('#multipicacao').click(function () {
-        valor.push("multipicacao");
-        $('.ixibicao').append("*");
-    })
-
-    $('#subtracao').click(function () {
-        valor.push("subtracao");
-        $('.ixibicao').append("-");
-    })
-
-    $('#adicao').click(function () {
-        valor.push("adicao");
-        $('.ixibicao').append("+");
-    })
-
-    $('#decimal').click(function () {
-        valor.push("decimal");
-        $('.ixibicao').append(".");
-    });
-
-    $('#zero').click(function () {
-        valor.push(0);
-        $('.ixibicao').append(0);
-    });
-
-    $('#um').click(function () {
-        valor.push(1);
-        $('.ixibicao').append(1);
-    });
-
-    $('#dois').click(function () {
-        valor.push(2);
-        $('.ixibicao').append(2);
-    });
-
-    $('#tres').click(function () {
-        valor.push(3);
-        $('.ixibicao').append(3);
-    });
-
-    $('#quatro').click(function () {
-        valor.push(4);
-        $('.ixibicao').append(4);
-    });
-
-    $('#cinco').click(function () {
-        valor.push(5);
-        $('.ixibicao').append(5);
-    });
-
-    $('#seis').click(function () {
-        valor.push(6);
-        $('.ixibicao').append(6);
-    });
-
-    $('#sete').click(function () {
-        valor.push(7);
-        $('.ixibicao').append(7);
-    });
-
-    $('#oito').click(function () {
-        valor.push(8);
-        $('.ixibicao').append(8);
-    });
-
-    $('#nove').click(function () {
-        valor.push(9);
-        $('.ixibicao').append(9);
-    });
-
+    function botaofuncao(){
+        $('#clear').click(function () {
+            $('.ixibicao').remove();
+            $('.resultado2').remove();
+            const novoresultado = $('<div class="resultado2"></div>');
+            const novaixibicao = $('<div class="ixibicao"></div>');
+            $('.vaga').append(novaixibicao, novoresultado);
+            valor = [];
+            numerostringantes = '';
+            numerostringdepois = '';
+        })
+    
+        $('#divisao').click(function () {
+            valor.push("divisao");
+            $('.ixibicao').append("/");
+        })
+    
+        $('#multipicacao').click(function () {
+            valor.push("multipicacao");
+            $('.ixibicao').append("*");
+        })
+    
+        $('#subtracao').click(function () {
+            valor.push("subtracao");
+            $('.ixibicao').append("-");
+        })
+    
+        $('#adicao').click(function () {
+            valor.push("adicao");
+            $('.ixibicao').append("+");
+        })
+    
+        $('#decimal').click(function () {
+            valor.push("decimal");
+            $('.ixibicao').append(".");
+        });
+    
+        $('#zero').click(function () {
+            valor.push(0);
+            $('.ixibicao').append(0);
+        });
+    
+        $('#um').click(function () {
+            valor.push(1);
+            $('.ixibicao').append(1);
+        });
+    
+        $('#dois').click(function () {
+            valor.push(2);
+            $('.ixibicao').append(2);
+        });
+    
+        $('#tres').click(function () {
+            valor.push(3);
+            $('.ixibicao').append(3);
+        });
+    
+        $('#quatro').click(function () {
+            valor.push(4);
+            $('.ixibicao').append(4);
+        });
+    
+        $('#cinco').click(function () {
+            valor.push(5);
+            $('.ixibicao').append(5);
+        });
+    
+        $('#seis').click(function () {
+            valor.push(6);
+            $('.ixibicao').append(6);
+        });
+    
+        $('#sete').click(function () {
+            valor.push(7);
+            $('.ixibicao').append(7);
+        });
+    
+        $('#oito').click(function () {
+            valor.push(8);
+            $('.ixibicao').append(8);
+        });
+    
+        $('#nove').click(function () {
+            valor.push(9);
+            $('.ixibicao').append(9);
+        });
+    }
 })
